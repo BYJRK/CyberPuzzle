456a45819c871a292177216aaa8845ef777a6699
Mikoto Suo <suomikoto@outlook.com>
2022-01-27 17:28:25 +0800

    remove IoC related code

--- CyberPuzzle/App.xaml.cs
+++ CyberPuzzle/App.xaml.cs
@@ -1,12 +1,5 @@
 ﻿using CyberPuzzle.ViewModel;
-using Microsoft.Extensions.DependencyInjection;
 using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows;
 
 namespace CyberPuzzle
@@ -16,28 +9,6 @@ namespace CyberPuzzle
     /// </summary>
     public partial class App : Application
     {
-        public IServiceProvider Services { get; }
 
-        public static new App Current => (App)Application.Current;
-
-        public App()
-        {
-            Services = ConfigureServices();
-        }
-
-        private static IServiceProvider ConfigureServices()
-        {
-            var services = new ServiceCollection();
-
-            services.AddTransient<MainWindowViewModel>();
-
-            return services.BuildServiceProvider();
-        }
-
-        private void Application_Startup(object sender, StartupEventArgs e)
-        {
-            var mWnd = new MainWindow();
-            mWnd.Show();
-        }
     }
 }
--- CyberPuzzle/ViewModel/MainWindowViewModel.cs
+++ CyberPuzzle/ViewModel/MainWindowViewModel.cs
@@ -5,10 +5,7 @@ using PropertyChanged;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Diagnostics;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Input;
 
 namespace CyberPuzzle.ViewModel
