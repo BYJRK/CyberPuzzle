9f9e0e22d217977b35b9c89bb956867b222789f7
suomi <suomi@AZURE-OFFICE>
2022-01-27 15:54:27 +0800

    添加项目文件。

---
+++ CyberPuzzle/Model/Piece.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using PropertyChanged;
+using System.Windows.Media;
+
+namespace CyberPuzzle.Model
+{
+    internal class Piece : ObservableObject
+    {
+        public string Word { get; set; }
+
+        /// <summary>
+        /// whether this button has been clicked and therefore cannot be clicked anymore
+        /// </summary>
+        public bool IsSelected { get; set; } = false;
+
+        /// <summary>
+        /// whether the button is enabled
+        /// </summary>
+        [DependsOn(nameof(IsSelected), nameof(IsAvailable))]
+        public bool IsEnabled => !IsSelected && IsAvailable;
+
+        /// <summary>
+        /// the coordinates of the current piece
+        /// </summary>
+        public (int X, int Y) Position { get; private set; }
+
+        /// <summary>
+        /// according to the rule, whether the current piece can be clicked
+        /// </summary>
+        public bool IsAvailable { get; set; }
+
+        [DependsOn(nameof(IsSelected))]
+        public Brush Foreground => IsSelected ? Brushes.Orange : Brushes.Black;
+
+        [DependsOn(nameof(IsEnabled))]
+        public Brush Background => IsEnabled ? Brushes.LightSkyBlue : Brushes.LightGray;
+
+        public Piece(string word, (int, int) pos)
+        {
+            Word = word;
+            Position = pos;
+        }
+    }
+}
---
+++ CyberPuzzle/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,99 @@
+﻿using CyberPuzzle.Model;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
+using PropertyChanged;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace CyberPuzzle.ViewModel
+{
+    internal class MainWindowViewModel : ObservableObject
+    {
+        public static readonly string[] Words = new[]
+        {
+             "1C", "55", "7A", "BD", "E9", "FF"
+        };
+
+        public int PuzzleSize => (int)Math.Sqrt(Puzzle.Count);
+
+        public ICommand PieceClickCommand { get; private set; }
+
+        public ObservableCollection<Piece> Puzzle { get; private set; } = new();
+
+        public ObservableCollection<string> SelectedWords { get; set; } = new();
+
+        [DoNotNotify]
+        public (int X, int Y) CurrentPosition { get; set; } = (-1, 0);
+
+        /// <summary>
+        /// the current direction, where true stands for horizontal
+        /// </summary>
+        [DoNotNotify]
+        public bool Direction { get; set; } = true;
+
+        public MainWindowViewModel()
+        {
+            GenerateRandomPuzzle(6, 1334, 5);
+            PieceClickCommand = new RelayCommand<Piece>(PieceClick);
+            UpdateAvailability();
+        }
+
+        private void UpdateAvailability()
+        {
+            foreach (var piece in Puzzle)
+            {
+                if (Direction && piece.Position.Y == CurrentPosition.Y)
+                    piece.IsAvailable = true;
+                else if (!Direction && piece.Position.X == CurrentPosition.X)
+                    piece.IsAvailable = true;
+                else
+                    piece.IsAvailable = false;
+            }
+        }
+
+        private void PieceClick(Piece p)
+        {
+            p.IsSelected = true;
+            SelectedWords.Add(p.Word);
+            CurrentPosition = p.Position;
+            Direction = !Direction;
+            UpdateAvailability();
+        }
+
+        private void GenerateRandomPuzzle(int size, int seed, int? wordCount = null)
+        {
+            var rnd = new Random(seed);
+
+            var words = (string[])Words.Clone();
+            Shuffle(words, rnd);
+            if (wordCount.HasValue)
+                words = words.Take(wordCount.Value).ToArray();
+            else
+                wordCount = Words.Length;
+            Puzzle.Clear();
+            for (int i = 0; i < size * size; i++)
+            {
+                Puzzle.Add(new Piece(words[rnd.Next(wordCount.Value)], (i % size, i / size)));
+            }
+        }
+
+        public static void Shuffle<T>(IList<T> list, Random rnd)
+        {
+            int n = list.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = rnd.Next(n + 1);
+                T value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
+        }
+    }
+}
